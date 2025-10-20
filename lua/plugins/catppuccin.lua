b30652e0a4ff3c2fd792552fde62d0683c1c56d7
tarik <yildirimtarikcan@gmail.com>
2025-10-20 11:49:20 -0400

    idk if this does anything but anyways

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,13 +5,16 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
   },
 
   -- Configure LazyVim to use catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
